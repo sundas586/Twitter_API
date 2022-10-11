8326b56c6485f231a69dbfa106f768ea586bb824
sundas586 <33677647+sundas586@users.noreply.github.com>
2022-10-10 19:40:17 -0700

    Create hidden.py

---
+++ hidden.py
@@ -0,0 +1,13 @@
+# API Key and Secret: Essentially the username and password for your App. 
+# Access Token and Secret: Access Tokens represent the user that you are making the request on behalf of. The ones that you can generate via the developer portal represent the user that owns the App
+
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "h7Lu...Ng",
+            "consumer_secret": "dNKenAC3New...mmn7Q",
+            "token_key": "10185562-eibxCp9n2...P4GEQQOSGI",
+            "token_secret": "H0ycCFemmC4wyf1...qoIpBo"}
